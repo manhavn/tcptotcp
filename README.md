22cd20b66a90e952fdded61ac064b6d7b9a39753
Bùi Văn Mạnh <manhavn@outlook.com>
2026-01-02 22:48:32 +0700

    init

---
+++ README.md
@@ -0,0 +1,80 @@
+# SETUP
+
+- Github: [https://github.com/manhavn/tcptotcp](https://github.com/manhavn/tcptotcp)
+- Crate: [https://crates.io/crates/tcptotcp](https://crates.io/crates/tcptotcp)
+
+```shell
+ cargo add tcptotcp
+```
+
+- `Cargo.toml`
+
+```toml
+# ...
+
+[dependencies]
+#tcptotcp = { git = "https://github.com/manhavn/tcptotcp.git" }
+tcptotcp = "0.0.1" # https://crates.io/crates/tcptotcp
+```
+
+- `test.rs`
+
+```rust
+#[cfg(test)]
+mod tests {
+    use tcptotcp::connect;
+    use crossbeam::channel::unbounded;
+    use std::net::{TcpListener, TcpStream};
+    use std::thread;
+    use std::time::Duration;
+
+    #[test]
+    fn test_tcp_server() {
+        let listener = TcpListener::bind("localhost:9000").unwrap();
+
+        let (tx, rx) = unbounded::<TcpStream>();
+        for stream in listener.incoming() {
+            match stream {
+                Ok(stream_client) => match rx.try_recv() {
+                    Ok(stream_app) => {
+                        thread::spawn(|| connect(stream_client, stream_app));
+                    }
+                    _ => {
+                        tx.send(stream_client).ok();
+                    }
+                },
+                Err(_) => {}
+            }
+        }
+    }
+
+    #[test]
+    fn test_tcp_client() {
+        let stream_server = TcpStream::connect("localhost:9000").unwrap();
+        let stream_app = TcpStream::connect("google.com:80").unwrap();
+
+        connect(stream_server, stream_app).unwrap();
+    }
+
+    #[test]
+    fn open_web_link() {
+        thread::sleep(Duration::from_secs(1));
+        let url = "http://localhost:9000";
+
+        #[cfg(target_os = "linux")]
+        std::process::Command::new("xdg-open")
+            .arg(url)
+            .spawn()
+            .unwrap();
+
+        #[cfg(target_os = "macos")]
+        std::process::Command::new("open").arg(url).spawn().unwrap();
+
+        #[cfg(target_os = "windows")]
+        std::process::Command::new("cmd")
+            .args(["/C", "start", url])
+            .spawn()
+            .unwrap();
+    }
+}
+```
---
+++ src/lib.rs
@@ -0,0 +1,110 @@
+use std::io::{Read, Result, Write};
+use std::net::{Shutdown, TcpStream};
+use std::sync::{
+    Arc,
+    atomic::{AtomicBool, Ordering},
+};
+use std::thread;
+use std::time::Duration;
+
+fn stream(
+    writer_closed: Arc<AtomicBool>,
+    reader_closed: Arc<AtomicBool>,
+    keep_alive_ping: Arc<AtomicBool>,
+    mut stream_read: TcpStream,
+    mut stream_write: TcpStream,
+) {
+    let mut buf: Vec<u8> = vec![0u8; 16 * 1024];
+    loop {
+        if reader_closed.load(Ordering::Relaxed) {
+            break;
+        } else {
+            match stream_read.read(&mut buf) {
+                Ok(0) => break,
+                Ok(n) => {
+                    if stream_write.write(&buf[..n]).is_err() {
+                        break;
+                    }
+                    keep_alive_ping.store(true, Ordering::Release);
+                }
+                Err(_) => break,
+            }
+        }
+    }
+    writer_closed.store(true, Ordering::Release);
+    stream_write.flush().ok();
+    stream_write.shutdown(Shutdown::Both).ok();
+}
+
+pub fn connect(stream_client: TcpStream, stream_app: TcpStream) -> Result<()> {
+    let clone_stream_client = stream_client.try_clone()?;
+    let clone_stream_app = stream_app.try_clone()?;
+    let clone_stream_client_closed = stream_client.try_clone()?;
+    let clone_stream_app_closed = stream_app.try_clone()?;
+
+    let worker1_closed = Arc::new(AtomicBool::new(false));
+    let worker2_closed = Arc::new(AtomicBool::new(false));
+    let worker1_closed_clone1 = Arc::clone(&worker1_closed);
+    let worker1_closed_clone2 = Arc::clone(&worker1_closed);
+    let worker1_closed_clone3 = Arc::clone(&worker1_closed);
+    let worker2_closed_clone1 = Arc::clone(&worker2_closed);
+    let worker2_closed_clone2 = Arc::clone(&worker2_closed);
+    let worker2_closed_clone3 = Arc::clone(&worker2_closed);
+
+    let keep_alive1 = Arc::new(AtomicBool::new(false));
+    let keep_alive2 = Arc::new(AtomicBool::new(false));
+    let ping_keep_alive_rw1 = Arc::clone(&keep_alive1);
+    let ping_keep_alive_rw2 = Arc::clone(&keep_alive2);
+    let ping_keep_alive_wo1 = Arc::clone(&keep_alive1);
+    let ping_keep_alive_wo2 = Arc::clone(&keep_alive2);
+
+    thread::spawn(move || {
+        let timeout: u64 = 5; // timeout rate 5s
+        let count_max_waiting = 7_200 / timeout; // waiting 2 hours { 60s * 60p * 2h = 7200s }
+        thread::sleep(Duration::from_secs(timeout)); // waiting first connection
+        let mut count_waiting: u64 = 0;
+        loop {
+            let ping1 = ping_keep_alive_rw1.load(Ordering::Acquire);
+            let ping2 = ping_keep_alive_rw2.load(Ordering::Acquire);
+            if ping1 && ping2 {
+                ping_keep_alive_rw1.store(false, Ordering::Release);
+                ping_keep_alive_rw2.store(false, Ordering::Release);
+                count_waiting = 0; // reset add count
+            } else {
+                count_waiting += 1; // add count
+                // waiting 2 hours
+                if count_waiting >= count_max_waiting {
+                    worker1_closed_clone3.store(true, Ordering::Release);
+                    worker2_closed_clone3.store(true, Ordering::Release);
+                    clone_stream_client_closed.shutdown(Shutdown::Both).ok();
+                    clone_stream_app_closed.shutdown(Shutdown::Both).ok();
+                    break;
+                }
+                thread::sleep(Duration::from_secs(timeout));
+
+                let worker1_is_closed = worker1_closed_clone3.load(Ordering::Acquire);
+                let worker2_is_closed = worker2_closed_clone3.load(Ordering::Acquire);
+                if worker1_is_closed || worker2_is_closed {
+                    break;
+                }
+            }
+        }
+    });
+    thread::spawn(move || {
+        stream(
+            worker1_closed_clone1,
+            worker2_closed_clone1,
+            ping_keep_alive_wo1,
+            clone_stream_app,
+            clone_stream_client,
+        )
+    });
+    stream(
+        worker1_closed_clone2,
+        worker2_closed_clone2,
+        ping_keep_alive_wo2,
+        stream_client,
+        stream_app,
+    );
+    Ok(())
+}
